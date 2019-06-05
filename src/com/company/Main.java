e828ac14227dc53685ef68505173dbbb3ee43528
violeta19 <violeta.ciceu19@gmail.com>
2019-06-05 21:42:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar='a';
+        byte myByte=123;
+        short myShort=12345;
+        float myFloat=12345;
+        String myString="avsfsgetdbdbu123456"; }
+}
